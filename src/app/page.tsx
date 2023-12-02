50ba602ca73b22891ad59a83c4a518e96a4d51ca
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-12-02 22:20:08 +0700

    fix minor UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
+'use client'
+
 import Image from 'next/image'
 import './page.scss'
-import Script from 'next/script'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-
+type Selection = 'GAMING' | 'ENTERPRISE' | 'UNKOWN'
+const REDIRECT_PAGE = "https://app.thinkmay.net/"
 
 export default function Home() {
+  const router = useRouter()
+  const [selection,setSelection] = useState<Selection>('ENTERPRISE')
+
+  useEffect(() => {
+    const result = localStorage.getItem('SELECTION')
+    switch (result) {
+      case 'ENTERPRISE':
+        setSelection('ENTERPRISE')
+        break;
+      case 'GAMING':
+        setSelection('UNKOWN')
+        // router.push(`${REDIRECT_PAGE}`)
+        break;
+      default:
+        break;
+    }
+  })
+
+  const gamingSelect = async () => {
+    localStorage.setItem('SELECTION','GAMING')
+    router.push(`${REDIRECT_PAGE}`)
+  }
+
+  const enterpriseSelect = async () => {
+    localStorage.setItem('SELECTION','ENTERPRISE')
+    setSelection('ENTERPRISE')
+  }
+
+
   return (
     <main className="main">
       <div className='content'>
-
-
         <div className='information'>
-          <section className=' section '>
             <header className='ctnHeader '>
               <div className='header contentMaxWidth'>
                 <div>
@@ -24,41 +54,57 @@ export default function Home() {
                   <li className='item'><a href="#technologies">Technologies</a></li>
                   <li className='item'><a href="#casestudy">Case study</a></li>
                   <li className='item'>
-                    <a className='btn-gaming' href="https://app.thinkmay.net/">Gaming</a>
+                    <a 
+                    onClick={gamingSelect}
+                    className='btn-gaming' 
+                    >Gaming</a>
                   </li>
                 </ul>
               </div>
             </header>
-            <div className='lookingFor contentMaxWidth'>
-              <h3 className='title'>Are you looking for</h3>
-              <div className='options flex ' >
-                <a href='https://app.thinkmay.net/' target='_blank' className='selection left'>
-                  <div className='relative mb-[40px]'>
-                    <Image alt='logo' priority={true} width={120} height={120} src={'/logo.png'} />
-                    <p className='w-[120px] absolute bottom-[-11%] left-[53%] italic'>Cloud gaming</p>
+
+            {
+              selection == 'UNKOWN' 
+              ? 
+                <section className=' section '>
+                  <div className='lookingFor contentMaxWidth'>
+                  <h3 className='title'>Are you looking for</h3>
+                  <div className='options flex ' >
+                    <a 
+                      href="javascript:void(0);"
+                      onClick={gamingSelect}
+                      className='selection left'>
+                      <div className='relative mb-[40px]'>
+                        <Image alt='logo' priority={true} width={120} height={120} src={'/logo.png'} />
+                        <p className='w-[120px] absolute bottom-[-11%] left-[53%] italic'>Cloud gaming</p>
+                      </div>
+                      <ul className='ctnText'>
+                        <li>Play game on any devices</li>
+                        <li>10$ monthly subscription</li>
+                        <li>Play any game instantly</li>
+                        <li>Available in Vietnam</li>
+                      </ul>
+                    </a>
+                    <a 
+                      href="javascript:void(0);"
+                      onClick={enterpriseSelect}
+                      className='selection right'>
+                      <div className='relative mb-[40px]'>
+                        <Image alt='logo' priority={true} width={120} height={120} src={'/logo.png'} />
+                        <p className='w-[120px] absolute bottom-[-11%] left-[53%] italic'>enterprise</p>
+                      </div>
+                      <ul className='ctnText'>
+                        <li>Consultant service</li>
+                        <li>Browser application</li>
+                        <li>Streaming protocol</li>
+                        <li>Virtualization technology</li>
+                      </ul>
+                    </a>
                   </div>
-                  <ul className='ctnText'>
-                    <li>Play game on any devices</li>
-                    <li>From 10$ per month</li>
-                    <li>Play game instantly</li>
-                    <li>Available in Vietnam</li>
-                  </ul>
-                </a>
-                <a href='#services' className='selection right'>
-                  <div className='relative mb-[40px]'>
-                    <Image alt='logo' priority={true} width={120} height={120} src={'/logo.png'} />
-                    <p className='w-[120px] absolute bottom-[-11%] left-[53%] italic'>enterprise</p>
-                  </div>
-                  <ul className='ctnText'>
-                    <li>Consultant CloudPc/gaming service</li>
-                    <li>Browser application</li>
-                    <li>Streaming protocol</li>
-                    <li>Virtualization technology</li>
-                  </ul>
-                </a>
-              </div>
-            </div>
-          </section>
+                </div>
+              </section>
+              : null
+            }
 
           <section className='enterprise'>
             <div id='services' className='  '>
@@ -66,7 +112,7 @@ export default function Home() {
                 <div className="left">
                   <div className='content section'>
                     <div className='mb-[80px]'>
-                      <h3 className='font-bold text-[5rem] text-white'>Cloud gaming <br />  4k120fps 60ms?</h3>
+                      <h3 className='font-bold text-[7rem] text-white'>Cloud gaming <br />  4k120fps 60ms?</h3>
                       <p className='mt-[40px] leading-none text-white text-[30px]'>
                         Are you building application which have
 
@@ -79,27 +125,29 @@ export default function Home() {
                   </div>
                   <div className='content section'>
                     <div className='mb-[80px]'>
-                      <h3 className='font-bold text-[5rem] text-white'>Streaming
+                      <h3 className='font-bold text-[7rem] text-white'>Virtualization
                         <br />infrastructure</h3>
                       <ul className='ctnText mt-[40px] pl-[5rem]'>
                         <li className='text'>Virtual machine?</li>
                         <li className='text'>Hypervisor?</li>
                         <li className='text'>Data center? </li>
                         <li className='text'>KVM or Hyper-V?</li>
-                        <li className='text'>Big word, let us handle that.</li>
+                        <a className='mt-[16px] btn-help' href="mailto:huyhoangdo@contact.thinkmay.net">Let us help you</a>
                       </ul>
                     </div>
                   </div>
                 </div>
                 <div className="right">
                   <div className='content section z-10'>
-                    <Image src={'/macbook.png'} alt='' priority={true} width={600} height={500} />
+                    <Image src={'/macbook.png'} alt='' priority={true} width={900} height={500} />
                   </div>
 
                   <div className='content section z-10'>
-                    <Image alt='logo' priority={true} width={600} height={120} src={'/server1.png'} />
+                    <Image alt='logo' priority={true} width={900} height={500} src={'/server1.png'} />
                   </div>
-                  <div className='stick'>
+                  <div 
+                    className='stick'
+                    >
                   </div>
                 </div>
               </div>
@@ -162,48 +210,49 @@ export default function Home() {
 
                 <div className='right'>
                   <div className='z-10 content'>
-                    <Image alt='logo' priority={true} width={600} height={120} src={'/macbook.png'} />
+                    <Image alt='logo' priority={true} width={900} height={120} src={'/macbook.png'} />
                   </div>
                   <div className="content">
 
                   </div>
                   <div className='z-10 content'>
-                    <Image alt='logo' priority={true} width={600} height={120} src={'/server2.png'} />
+                    <Image alt='logo' priority={true} width={900} height={120} src={'/server2.png'} />
                   </div>
 
                   <div className="stick">
 
                   </div>
                 </div>
-
-                {/*<div className='bg'>
-                </div>*/}
-
-                {/*<div className="bg2">
-
-                </div>*/}
               </div>
 
               {/* Our Client */}
               <div id='casestudy' className='subContent section'>
                 <h2 className='title'>Our Client</h2>
 
-                <div className='w-full grid grid-cols-3 gap-16'>
-                  <div className='client'>
+                <div 
+                  className='w-full grid grid-cols-3 gap-16'
+                >
+                  <a
+                    href="https://phase-somersault-5b1.notion.site/Bright-Cloud-4b173effb5ff49f1a85134327162a945?pvs=4"
+                    className='client'>
                     <div>
                       <Image priority={true} width={150} height={100} src={'/brightcloud.png'} alt='' />
                     </div>
                     <p>Check out how to we power browser cloud gaming for BrightCloud</p>
-                  </div>
-                  <div className='client'>
+                  </a>
+                  <a
+                    href="https://phase-somersault-5b1.notion.site/Thinkmay-cloud-gaming-6699b79995464ab981295137cfba3533?pvs=4"
+                    className='client'>
                     <div>
                       <Image priority={true} width={150} height={100} src={'/logo.png'} alt='' />
                     </div>
                     <p>Checkout how we use
                       our WebRTC, D3D11 and NvEnc expertise
                       to power our cloud gaming service</p>
-                  </div>
-                  <div className='client'>
+                  </a>
+                  <a
+                    href="https://phase-somersault-5b1.notion.site/True-Cloud-9c1a033f6c4e4f768030fabe586cf381?pvs=4"
+                    className='client'>
                     <div>
                       <Image priority={true} width={150} height={100} src={'/truecloud.png'} alt='' />
                     </div>
@@ -212,7 +261,7 @@ export default function Home() {
                       cloud gaming service with KVM and
                       thinkmay
                       remote desktop</p>
-                  </div>
+                  </a>
                 </div>
               </div>
               {/* contact */}
@@ -257,16 +306,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-37TE72KVQ1" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-37TE72KVQ1');
-        `}
-      </Script>
       </div>
     </main>
   )
